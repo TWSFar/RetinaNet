b6b7e175d647a8d78776f24b92be57157391f358
TWSFar <twsfcz@163>
2019-11-17 17:56:21 +0800

    update

--- mypath.py
+++ mypath.py
@@ -1,12 +1,17 @@
+import os
+
+
 class Path(object):
     @staticmethod
+    user_dir = os.path.expanduser('~')
+
     def db_root_dir(dataset):
         if dataset == 'voc':
-            return '/home/twsf/work/DSSD/data/VOC2012'
+            return user_dir + '/work/DSSD/data/VOC2012'
         elif dataset == 'coco':
-            return '/home/twsf/data/COCO'
+            return user_dir + '/data/COCO'
         elif dataset == 'visdrone':
-            return '/home/twsf/data/Visdrone'
+            return user_dir + '/data/Visdrone'
         else:
             print('Dataset {} not available.'.format(dataset))
             raise NotImplementedError
